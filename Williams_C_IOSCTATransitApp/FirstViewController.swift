a7bbf379bb00627b241bdc8a9ab46a240f0315d2
CWILL <cwill@loop-depaulsecure-235-15.depaulsecure-student.depaul.edu>
2018-04-26 16:47:39 -0500

    Initial Commit

---
+++ Williams_C_IOSCTATransitApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Williams_C_IOSCTATransitApp
+//
+//  Created by CWILL on 4/26/18.
+//  Copyright © 2018 DePaul. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
